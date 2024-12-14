3d7a86232bdcb020074adc317d3d93c254fc98c3
Prinkal <154504822+PrinkalDhingra@users.noreply.github.com>
2024-12-14 21:44:14 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import CampaignAutomation from "./pages/CampaignAutomation";
+import PerformanceAnalytics from "./pages/AnalyticsDashboard";
 //import Personalization from './components/Personalization';
 
 const App = () => {
@@ -9,6 +13,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/personalization" element={<Personalization />} /> */}
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/campaignautomation" element={<CampaignAutomation/>} />
+        <Route path="/analytics" element={<PerformanceAnalytics/>} />
       </Routes>
     </Router>
   );
